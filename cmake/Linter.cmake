d7c1c3067bbebf06485b7d72b1679bca64c6dc04
Julian A Rendon <julianrendon514@gmail.com>
2026-03-07 05:12:52 +0000

    Implement pre-commit config and updating linting.

--- cmake/Linter.cmake
+++ cmake/Linter.cmake
@@ -1,6 +1,7 @@
 if(BUILD_TESTING)
   find_package(ament_lint_auto REQUIRED)
 
+  set(ament_cmake_uncrustify_CONFIG_FILE "${CMAKE_CURRENT_SOURCE_DIR}/cmake/.uncrustify.cfg")
   set(AMENT_LINT_AUTO_EXCLUDE
     third_party
   )
