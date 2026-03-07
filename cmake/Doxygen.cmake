23d62c7a265041e0b008597b3e2333d8e5212bac
Julian A Rendon <julianrendon514@gmail.com>
2026-03-07 02:47:48 +0000

    Update linting tests when package is being built.

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -1,16 +1,26 @@
 find_package(Doxygen)
 
-if(DOXYGEN_FOUND)
+if(BUILD_DOC AND DOXYGEN_FOUND)
     set(DOXYGEN_OUTPUT_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/docs")
-    set(DOXYFILE_IN "${CMAKE_CURRENT_SOURCE_DIR}/cmake/Doxyfile.in")
+    set(DOXYGEN_USE_MDFILE_AS_MAINPAGE "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
+    set(DOXYGEN_WARN_IF_UNDOCUMENTED YES)
+    set(DOXYGEN_WARN_IF_DOC_ERROR YES)
+    set(DOXYGEN_WARN_NO_PARAM_DOC YES)
+    set(DOXYGEN_RECURSIVE YES)
+    set(DOXYGEN_FILE_PATTERNS "*.hpp" "*.cpp" "*.h" "*.c")
+    set(DOXYGEN_EXCLUDE_PATTERNS "*/third_party/*")
+    set(DOXYGEN_GENERATE_HTML YES)
+    set(DOXYGEN_GENERATE_LATEX NO)
+    set(DOXYGEN_HAVE_DOT YES)
+    set(DOXYGEN_EXTRACT_ALL NO) 
+    set(DOXYGEN_CLASS_DIAGRAMS YES)
     doxygen_add_docs(docs
-    "${CMAKE_CURRENT_SOURCE_DIR}/include"
-    "${CMAKE_CURRENT_SOURCE_DIR}/src"
-    CONFIG_FILE "${DOXYFILE_IN}"
-    COMMENT "[${PROJECT_NAME}]Generating API documentation"
+        "${CMAKE_CURRENT_SOURCE_DIR}/include"
+        "${CMAKE_CURRENT_SOURCE_DIR}/src"
+        "${CMAKE_CURRENT_SOURCE_DIR}/README.md"
+        COMMENT "[${PROJECT_NAME}] Generating API documentation with validation."
     )
-
-    message(STATUS "Documentation target created.")
+    message(STATUS "[${PROJECT_NAME}] Documentation target 'docs' created.")
 
 elseif(BUILD_DOC AND NOT DOXYGEN_FOUND)
     message(WARNING "[${PROJECT_NAME}] Doxygen not found.")
--- cmake/Linter.cmake
+++ cmake/Linter.cmake
@@ -3,8 +3,9 @@ if(BUILD_TESTING)
 
   set(AMENT_LINT_AUTO_EXCLUDE
     third_party
-    )
+  )
 
+  # TODO: Remove once Copyrights are established.
   set(ament_cmake_copyright_FOUND TRUE)
   set(ament_cmake_cpplint_FOUND TRUE)
   ament_lint_auto_find_test_dependencies()
