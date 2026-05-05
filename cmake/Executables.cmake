534588e51b49678e901df5246ff31269f2acca92
Julian A Rendon <julianrendon514@gmail.com>
2026-05-05 18:42:25 -0400

    Rename python nodes.

--- cmake/Executables.cmake
+++ cmake/Executables.cmake
@@ -2,5 +2,5 @@
 set(CAMERA_SERVER_EXE_NAME camera_server_node)
 
 # Python Nodes
-set(STEERING_SERVO_EXE_NAME steering_servo_node.py)
-set(MOTOR_CONTROL_EXE_NAME motor_control_node.py)
+set(DRIVE_CONTROLLER_EXE_NAME drive_controller)
+set(STEERING_CONTROLLER_EXE_NAME steering_controller)
